d79b22b30b7acd3da0c4a876e1b10ff48a0db809
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-06-08 23:34:53 +0100

    feats: added navbar and routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Routing from './Component/routes/routes'
+import Navbar from './Component/Navbar/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <Routing />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routing />
+      </div>
+    </Router>
   )
 }
 
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Navbar() {
+    return (
+        <div>
+            This is the navbar
+        </div>
+    )
+}
